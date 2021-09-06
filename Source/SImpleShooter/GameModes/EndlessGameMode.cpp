4d78d5e20706e1d9fb3c37a2d3df40a1b1b217ec
SloanGwaltney <sloangwaltney2@gmail.com>
2021-09-06 04:46:26 -0400

    start implementation of endless game mode

--- Source/SImpleShooter/GameModes/EndlessGameMode.cpp
+++ Source/SImpleShooter/GameModes/EndlessGameMode.cpp
@@ -2,4 +2,72 @@
 
 
 #include "EndlessGameMode.h"
+#include "GameFramework/Controller.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/PlayerStart.h"
+#include "SImpleShooter/Characters/ShooterCharacter.h"
 
+void AEndlessGameMode::PawnKilled(APawn* PawnKilled) 
+{
+    Super::PawnKilled(PawnKilled);
+    APlayerController* PlayerController = Cast<APlayerController>(PawnKilled->GetController());
+    if (PlayerController != nullptr)
+    {
+        EndGame();
+        return;
+    }
+    NumberOfEnemiesKilled = NumberOfEnemiesKilled + 1;
+    NumberOfEnemiesRemaining = NumberOfEnemiesRemaining - 1;
+    if (NumberOfEnemiesRemaining <= 0)
+    {
+        StartRound();
+    }
+    AShooterCharacter* CastedPawn = Cast<AShooterCharacter>(PawnKilled);
+    CastedPawn->Destroy();
+}
+
+void AEndlessGameMode::StartPlay() 
+{
+    Super::StartPlay();
+    StartRound();
+}
+
+int32 AEndlessGameMode::GetNumberOfSecondsUntilWaveStart() 
+{
+    return SecondsUntilWaveStart;
+}
+
+void AEndlessGameMode::EndGame() 
+{
+    UGameplayStatics::OpenLevel(GetWorld(), "WeaponSelectionLevel");
+}
+
+void AEndlessGameMode::StartRound() 
+{
+    WaveCounter = WaveCounter + 1;
+    SecondsUntilWaveStart = 5;
+    GetWorldTimerManager().SetTimer(WaveStartCountdownHandle, this, &AEndlessGameMode::WaveStartCountdownStep, 1.f, true);
+}
+
+void AEndlessGameMode::SpawnEnemies() 
+{
+    NumberOfEnemiesInWave = WaveCounter;
+    NumberOfEnemiesRemaining = NumberOfEnemiesInWave;
+    TArray<AActor*> SpawnPoints;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), APlayerStart::StaticClass(), SpawnPoints);
+    for (int32 x = 0; x < NumberOfEnemiesInWave; x++)
+    {
+        AActor* SpawnPointToUse = SpawnPoints[FMath::RandRange(0, SpawnPoints.Num() - 1)];
+        GetWorld()->SpawnActor<AActor>(AIClassToSpawn, SpawnPointToUse->GetActorLocation(), SpawnPointToUse->GetActorRotation());
+    }
+}
+
+void AEndlessGameMode::WaveStartCountdownStep() 
+{
+    SecondsUntilWaveStart = SecondsUntilWaveStart -1;
+    if (SecondsUntilWaveStart <= 0)
+    {
+        GetWorldTimerManager().ClearTimer(WaveStartCountdownHandle);
+        SpawnEnemies();
+    }
+}
--- Source/SImpleShooter/GameModes/EndlessGameMode.h
+++ Source/SImpleShooter/GameModes/EndlessGameMode.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "SImpleShooter/SImpleShooterGameModeBase.h"
+#include "SImpleShooter/Characters/ShooterCharacter.h"
 #include "EndlessGameMode.generated.h"
 
 /**
@@ -13,5 +14,27 @@ UCLASS()
 class SIMPLESHOOTER_API AEndlessGameMode : public ASImpleShooterGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void PawnKilled(APawn* PawnKilled) override;
+	virtual void StartPlay();
+
+	UFUNCTION(BlueprintPure)
+	int32 GetNumberOfSecondsUntilWaveStart();
+
+private:
+	void EndGame();
+	void StartRound();
+	void SpawnEnemies();
+	void WaveStartCountdownStep();
+
+	int32 NumberOfEnemiesKilled = 0;
+	int32 NumberOfEnemiesInWave = 1;
+	int32 NumberOfEnemiesRemaining = 0;
+	int32 SecondsUntilWaveStart = 0;
+	int32 WaveCounter = 0;
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<AShooterCharacter> AIClassToSpawn;
+
+	FTimerHandle WaveStartCountdownHandle;
 };
