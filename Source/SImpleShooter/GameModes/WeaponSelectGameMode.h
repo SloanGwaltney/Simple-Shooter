e4715e98699ab7c42dfb738ec5102c0ee3f37c08
SloanGwaltney <sloangwaltney2@gmail.com>
2021-09-10 06:58:31 -0400

    new game mode

---
+++ Source/SImpleShooter/GameModes/WeaponSelectGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SImpleShooter/SImpleShooterGameModeBase.h"
+#include "WeaponSelectGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLESHOOTER_API AWeaponSelectGameMode : public ASImpleShooterGameModeBase
+{
+	GENERATED_BODY()
+	
+};
